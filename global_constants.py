322f1157172d52ce2d7eb8dd0a14e4519385d8a9
Plykin K. R <KPlykin@sfu-kras.ru>
2021-10-13 14:37:27 +0700

    v 0.2
    Добавлены функции для формирования ИД, а также необходимые глобальные константы.

--- global_constants.py
+++ global_constants.py
@@ -9,4 +9,46 @@ NUM_CON_ALM = 700
 # Условное значение, которое определяет, что данная строка не может передоваться
 # в соот. сигнале
 NOT_BEL_STR = 999999999
-
+# Словарь, который содержит сопоставление индекса сигнала с его именем
+DIC_IND_SIG_IN_NAME = {
+                       0 : ALL_SIGNALS[0]
+                      ,1 : ALL_SIGNALS[1]
+                      ,2 : ALL_SIGNALS[2]
+                      ,3 : ALL_SIGNALS[3]
+                      }
+# Словарь, который содержит сопоставление имени сигнала с его индексом
+DIC_NAME_SIG_IN_IND = {
+                       ALL_SIGNALS[0] : 0
+                      ,ALL_SIGNALS[1] : 1
+                      ,ALL_SIGNALS[2] : 2
+                      ,ALL_SIGNALS[3] : 3
+                      }
+# Слорваь, который содержит сопоставление номера строки с индексом (по столб.)
+# в матрице
+DIC_NUM_STR_IN_IND = {
+                      ALL_STRINGS[0] : 0
+                     ,ALL_STRINGS[1] : 1
+                     ,ALL_STRINGS[2] : 2
+                     ,ALL_STRINGS[3] : 3
+                     ,ALL_STRINGS[4] : 4
+                     ,ALL_STRINGS[5] : 5
+                     ,ALL_STRINGS[6] : 6  
+                     }
+# Слорваь, который содержит сопоставление индекса (по столб.) в матрице с
+# номером строки
+DIC_IND_STR_IN_STR = {
+                      0 : ALL_STRINGS[0]
+                     ,1 : ALL_STRINGS[1]
+                     ,2 : ALL_STRINGS[2]
+                     ,3 : ALL_STRINGS[3]
+                     ,4 : ALL_STRINGS[4]
+                     ,5 : ALL_STRINGS[5]
+                     ,6 : ALL_STRINGS[6]  
+                     }
+# Словарь, который содержит сопоставление индекса строки со списокм индексов
+# сигналов, которые не могут содержать данную строку
+DIC_IND_STR_IN_IND_SIG_NOT_BEL = {
+                                  DIC_NUM_STR_IN_IND[13] : [DIC_NAME_SIG_IN_IND["L3OC"], DIC_NAME_SIG_IN_IND["L1OC"]]
+                                 ,DIC_NUM_STR_IN_IND[50] : [DIC_NAME_SIG_IN_IND["L3OC"], DIC_NAME_SIG_IN_IND["L1SC"], DIC_NAME_SIG_IN_IND["L2SC"]]
+                                 }
+                                 
--- support_function.py
+++ support_function.py
@@ -1,4 +1,4 @@
-from numpy import matrix, array
+from numpy import matrix, array, empty
 from random import shuffle
 
 import global_constants as gc
@@ -36,6 +36,140 @@ def formation_matrix_units(count_str: int, vec_count_units_for_str: list):
   mat_units = matrix(list_for_matr)
   return mat_units
   
+def formation_vec_l_psk(start_len: int, step_len: int):
+  """
+  Функция формирования вектора с ИД, который характеризует размер ПСК для каждого
+  сигнала.
+
+  Вх. аргументы:
+    # start_len - стартовый размер ПСК (не может быть < 3)
+    # step_len  - шаг размера ПСК (не может быть < 0)
+
+  Вых. аргументы: 
+    # vec_l_psk - вектор, который хранит в себе разрмер ПСК для каждого сигнала.
+                  Если стартовый размер не соот. входным данным, то объект будет
+                  None
+  """
+  vec_l_psk = None
+  if ((start_len >= 3) and (step_len >= 0)):
+    vec_l_psk = [start_len+(i*step_len) for i in range(gc.ALL_SIGNALS.size)]
+    vec_l_psk = array(vec_l_psk)
+  return vec_l_psk
+
+def formation_vec_t(start_val: int, step_val: int):
+  """
+  Функция формирования вектора с ИД, который характеризует интервал актуальности
+  опред. параметра для каждого сигнала.
+
+  Вх. аргументы: 
+    # start_val - стартовое значение  (не может быть < 0)
+    # step_val  - шаг по времени (не может быть < 0)
+
+  Вых. аргументы:
+    # vec_t - вектор с ИД, который характеризует интервал актуальности.
+              Если не соот. входным условиями, то объет будет None 
+  """
+  vec_t = None
+  if ((start_val >= 0) and (step_val >= 0)):
+    vec_t = [start_val+(i*step_val) for i in range(gc.ALL_SIGNALS.size)]
+    vec_t = array(vec_t)
+  return vec_t
+
+def formation_mat_raw_data(start_val: int, step_mat_str: int, step_mat_col: int, max_val = 9999999):
+  """
+  Функция формирования матриц для необходим. ИД
+
+  Вх. аргументы:
+    # start_val    - начальное значение (не может быть < 0)
+    # step_mat_str - шаг по времени между строками матрицы (не может быть  < 0)
+    # step_mat_col - шаг по времени между столбцами матрицы (не может быть  < 0)
+    # max_val      - максимальное значение параметра 
+
+  Вых. аргументы:
+    # mat_output - сформированная матрица
+                  Если не соот. входным условиями, то объет будет None
+  """
+  mat_output = empty([gc.ALL_STRINGS.size, gc.ALL_SIGNALS.size], dtype=int)
+  if ((start_val >=0) and (step_mat_str >= 0) and (step_mat_col >= 0)): 
+    # Цикл перебора строк
+    for n_str in gc.ALL_STRINGS:
+      # Получаем соот. индекс строки
+      ind_str = gc.DIC_NUM_STR_IN_IND[n_str]
+      # Получаем список, который содержит индексы сигналов, которые не имеют
+      # данную строку
+      list_not_bel_str = gc.DIC_IND_STR_IN_IND_SIG_NOT_BEL.get(ind_str)
+      # Цикл по сигналами
+      for n_sig in gc.ALL_SIGNALS:
+        # Получаем соот. индекс сигнала
+        ind_sig = gc.DIC_NAME_SIG_IN_IND[n_sig]
+        # Проверяем, имеет ли данный сигнал данную строку, если нет, то записыв.
+        # определенное число в данный элемент, иначе заполняем матрицу
+        if ((list_not_bel_str != None) and (ind_sig in list_not_bel_str)):
+          mat_output[ind_str, ind_sig] = gc.NOT_BEL_STR
+        else:
+          val = start_val+(ind_str*step_mat_str)+(ind_sig*step_mat_col)
+          if (val > max_val):
+            val -= max_val
+          mat_output[ind_str, ind_sig] = val
+  else:
+    mat_output = None
+  return mat_output
+
+def formation_raw_data():
+  """
+  Функция формирования исходных данных для моделирования
+
+  Вых. аргументы:
+    ### Параметры для сигналов ###
+
+    # vec_l_psk - Каждый эл. в данном векторе характеризует размер ПСК для соот. сигнала
+    # vec_t_oi  - Каждый эл. в данном векторе характеризует интервал актуальности строк с ОИ
+    # vec_t_as  - Каждый эл. в данном векторе характеризует интервал актуальности строк с АС
+
+    ### Параметры для строк ###
+
+    # mat_t_str - Матрица характеризует интервал актуальности строки опред. сигнала
+    # mat_kps   - Матрица характеризует количество повторов строк для опред. сигнала
+    # mat_sync  - Матрица характеризует значение синхронизации строки для опред. сигнала
+    # mat_pps   - Матрица характеризует значение периода повторения строки для опред. сигнала
+    # mat_prior - Матрица характеризует значение приоритета строки для опред. сигнала
+    # mat_pvs   - Матрица характеризует порядок включения строки для опред. сигнала
+  """
+  # Начальные параметры для ИД
+  start_l_psk = 14
+  start_t_oi  = 0
+  start_t_as  = 2000
+  start_t_str = 0
+  start_kps   = 11
+  start_sync  = 0
+  start_pps   = 0
+  start_prior = 5
+  start_pvs   = 0
+  # Шаги для ИД
+  step_l_psk = 1
+  step_t_oi  = 0
+  step_t_as  = 1000
+  step_str_t_str = 200 ; step_col_t_str = 100 
+  step_str_kps   = 10  ; step_col_kps   = 1 
+  step_str_sync  = 3   ; step_col_sync  = 1 
+  step_str_pps   = 3   ; step_col_pps   = 1
+  step_str_prior = 10  ; step_col_prior = 2 
+  step_str_pvs   = 0   ; step_col_pvs   = 0
+
+  # Формируем ИД
+  vec_l_psk = formation_vec_l_psk(start_l_psk, step_l_psk) 
+  vec_t_oi  = formation_vec_t(start_t_oi, step_t_oi)
+  vec_t_as  = formation_vec_t(start_t_as, step_t_as)
+  mat_t_str = formation_mat_raw_data(start_t_str, step_str_t_str, step_col_t_str)
+  mat_kps   = formation_mat_raw_data(start_kps, step_str_kps, step_col_kps)
+  mat_sync  = formation_mat_raw_data(start_sync, step_str_sync, step_col_sync, 15)
+  mat_pps   = formation_mat_raw_data(start_pps, step_str_pps, step_col_pps, 15)
+  mat_prior = formation_mat_raw_data(start_prior, step_str_prior, step_col_prior)
+  mat_pvs   = formation_mat_raw_data(start_pvs, step_str_pvs, step_col_pvs)
+
+  return vec_l_psk, vec_t_oi, vec_t_as, \
+         mat_t_str, mat_kps , mat_sync, mat_pps, mat_prior, mat_pvs
+
 def formation_std_raw_data():
   """
   Функция формирования исходных данных, необходимых для моделирования при условии
@@ -43,6 +177,10 @@ def formation_std_raw_data():
     count_sat (КА в системе)                   = 24
     count_sig (кол-во сигналов у каждого КА)   =  4
     count_str (кол-во строк у каждого сигнала) =  7
+  
+  Порядок строк и сигналов след.
+  сигналы: "L3OC", "L2SC", "L1SC", "L1OC"
+  строки:  13, 16, 25, 31, 32, 50, 60
 
   Вых. агументы: 
     ### Параметры, которые формируются другой программой ###
